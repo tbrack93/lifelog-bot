2e45487e197785cac1a67ddafa346cd04e03cf0a
Yuriy Plugatariov <plugatariov@gmail.com>
2017-03-09 17:45:36 +0200

    Add starter bot files

--- config/application.rb
+++ config/application.rb
@@ -15,11 +15,14 @@ require "sprockets/railtie"
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
+Dotenv::Railtie.load
 
 module LifelogBot
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.paths.add File.join('app', 'bot'), glob: File.join('**', '*.rb')
+    config.autoload_paths += Dir[Rails.root.join('app', 'bot', '*')]
   end
 end
